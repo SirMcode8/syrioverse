9ed324a653daa12699cd1e683b63d9e80eb1d996
SirMcode8 <marksirias8@gmail.com>
2025-06-17 15:30:43 +1000

    Create CameraActivity.kt

---
+++ CameraActivity.kt
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    android:padding="20dp">
+
+    <TextView
+        android:id="@+id/infoText"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Loading..."
+        android:textSize="16sp"
+        android:padding="10dp" />
+
+    <Button
+        android:id="@+id/openCameraBtn"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Use Camera" />
+
+    <Button
+        android:id="@+id/chatWithAIBtn"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Chat with AI" />
+
+    <Button
+        android:id="@+id/connectWorldBtn"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Connect with the World" />
+
+</LinearLayout>
